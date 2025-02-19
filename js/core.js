4e44b4ed90719023f166663a293505637091f3c6
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-20 01:15:52 +1000

    Added Player And CPU Moves

--- js/core.js
+++ js/core.js
@@ -1,20 +1,15 @@
 // DOM
 const BUT_CELLS = document.querySelectorAll("button.cell");
+const ACTIVE_PLAYER_VALUE = document.querySelector("#active-player-value");
 
 // Events
 BUT_CELLS.forEach(button => {
   button.addEventListener("click", () => {
     const INDEX = button.dataset.id;
     console.log(INDEX);
-    updateCellUI(button);
-    gameManager.playMove(INDEX);
+    gameManager.play(INDEX);
   })
 })
-
-function updateCellUI(button){
-  if (button.textContent !== "") return;
-  button.textContent = gameManager.activePlayer.symbol;
-}
 
 // Core Logic
 class GameManager {
@@ -24,34 +19,76 @@ class GameManager {
     this.gameBoard = new GameBoard();
   }
 
-  getActivePlayer() {
+  getActivePlayerName() {
     return this.activePlayer.getName();
   }
 
   changeActivePlayer() {
     this.activePlayer = this.activePlayer === this.PLAYERS[0] ? this.PLAYERS[1] : this.PLAYERS[0];
+    this.updateActivePlayerUI();
+  }
+  
+  updateActivePlayerUI() {
+    ACTIVE_PLAYER_VALUE.innerHTML = `<b>Turn:</b> ${this.getActivePlayerName()}`;
   }
 
-  playMove(index) {
+  play(index) {
+    this.playerMove(index);
+    
+    if (this.gameBoard.getMovesLeft() > 1) {
+      this.cpuMove();
+    }
+    else {
+      console.log("Gameover");
+      console.log(this.gameBoard.getMovesLeft());
+    }
+  }
+
+  playerMove(index) {
     const CELL = this.gameBoard.board[index];
     if (CELL.getValue() != undefined) return;
 
     CELL.setValue(this.activePlayer);
+    this.updateCellUI(index);
     this.changeActivePlayer();
+    this.gameBoard.decMovesLeft();
   }
 
-  playRandom() {
-    
+  cpuMove() {
+    console.log("Cpu move");
+    // Map available array cells as indexes and filter out what is already taken
+    let cellIndexes = this.gameBoard.board.map((cell, index) => cell.getValue() === undefined ? index : null)
+    let availableCells = cellIndexes.filter(index => index !== null);
+    const RANDOM_INDEX = availableCells[Math.floor(Math.random() * availableCells.length)];
+    this.gameBoard.board[RANDOM_INDEX].setValue(this.activePlayer);
+    this.updateCellUI(RANDOM_INDEX);
+    this.changeActivePlayer();
+    this.gameBoard.decMovesLeft();
   }
+
+  updateCellUI(index) {
+    if (BUT_CELLS[index].textContent !== "") return;
+    BUT_CELLS[index].textContent = gameManager.activePlayer.symbol;
+  }
+  
 }
 
 class GameBoard {
   constructor () {
     this.board = this.createBoard();
+    this.movesLeft = 9;
   }
 
   createBoard() {
     return Array(9).fill(undefined).map(() => new Cell());
+  }
+
+  decMovesLeft() {
+    this.movesLeft--;
+  }
+
+  getMovesLeft() {
+    return this.movesLeft;
   }
 }
 
@@ -82,3 +119,4 @@ class Player {
 
 // Create Board
 let gameManager = new GameManager();
+gameManager.updateActivePlayerUI();
